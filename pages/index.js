b628e235950bb20eba600e950160ba726adc2a64
vishy-dev <agarwal.vrishank@gmail.com>
2021-03-22 22:17:42 +0000

    fix(pages/index.js): change Description

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
- <div className={`font-bold text-2xl`}>Vatsal is decent?!</div>
+ <div className={`font-bold text-2xl`}>Hello World!</div>
   )
 }
